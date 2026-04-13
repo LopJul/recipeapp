8ce0deb777eb7d91d381286ea9797c854ce5a476
Julia Loponen <julia.loponen@myy.haaga-helia.fi>
2026-04-13 22:42:59 +0300

    update

--- src/main/java/myproject/recipeapp/service/UserDetailServiceImpl.java
+++ src/main/java/myproject/recipeapp/service/UserDetailServiceImpl.java
@@ -23,13 +23,14 @@ public class UserDetailServiceImpl implements UserDetailsService {
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {   
     	User curruser = repository.findByUsername(username);
-        UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
+
+      if (curruser == null) {
+        throw new UsernameNotFoundException("User not found: " + username);
+    }
+      
+        return new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
         		AuthorityUtils.createAuthorityList("ROLE_" + curruser.getRole().name()));
-
-            
-        return user;
-
         
-    }   
+    };
   
 }
